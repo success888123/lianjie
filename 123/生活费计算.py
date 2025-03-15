54dfeebd5a77668a2e5a6e49fdc6cef63061f08c
success888123 <3181670090@qq.com>
2025-03-15 22:04:16 +0800

    Add files via upload

---
+++ 123/生活费计算.py
@@ -0,0 +1,10 @@
+name=input('请输入项目')
+z=int(input('请输入所花金额'))
+x=900-z
+name1=input('请输入项目')
+z1=int(input('请输入金额'))
+x1=600-z1
+sheng=1500-z-z1
+print(name,'剩余',x)
+print(name1,'剩余',x1)
+print('生活费剩余',sheng)
